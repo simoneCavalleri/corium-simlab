0af3ac07705206d486d74ae540761e70d86ba826
Simone Cavalleri <simone.cavalleri.94@gmail.com>
2026-08-02 13:02:31 +0200

    refactor(simlab): remove demo telemetry service and sample meshes

--- src/services/TelemetryService.cpp
+++
@@ -1,7 +0,0 @@
-#include "corium_sim/services/TelemetryService.hpp"
-
-namespace corium_sim::services {
-
-// Non-template non-inline explicit instantiations or helpers if required.
-
-} // namespace corium_sim::services
